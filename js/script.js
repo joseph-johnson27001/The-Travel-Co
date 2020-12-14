ab3113e15c1ec563b61b25ce3b07ecc5c15f746b
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2020-12-14 11:01:51 +0000

    Scroll To Top Button
    - Added functionality for bottom button to scroll back to top of page on click

--- js/script.js
+++ js/script.js
@@ -5,15 +5,21 @@ VARIABLES
 const btnContainer = document.getElementById("navbarNav")
 const btns = btnContainer.getElementsByClassName("nav-link")
 
+// BOTTOM SCROLL TO TOP BUTTON
+const backToTopButton = document.getElementById("back-to-top-button");
 
+/* =============================================================================
+EVENT LISTENERS
+================================================================================ */
 
+// Event listener for scroll to top button
+backToTopButton.addEventListener("click", scrollTop)
 
 /* =============================================================================
 FUNCTIONS
 ================================================================================ */
 
 // NAVIGATION FUNCTIONS
-// =============================================================================
 
 // Function to give targeted button active status
 for (var i = 0; i < btns.length; i++) {
@@ -23,11 +29,16 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-
 // Function to remove active class on nav when clicking on screen BUTTONS
 function removeActiveNavClass() {
   let current = document.getElementsByClassName("active");
   if (current.length > 0) {
     current[0].className = current[0].className.replace("active", "");
   }
+}
+
+// BOTTOM BUTTON SCROLL TO TOP FUNCTION
+
+function scrollTop() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
 }
