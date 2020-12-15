45cc3066a8ddbd8b59e69549246c1e63e4d6e7ec
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2020-12-15 14:13:12 +0000

    Added functionality and cleaner code to left and right activities buttons functions
    - Created function rightButton()
    - Created function leftButton()
    - Created function fadeOutImages()
    - Created function fadeInImages()

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ VARIABLES
 const btnContainer = document.getElementById("navbarNav")
 const btns = btnContainer.getElementsByClassName("nav-link")
 
+//ACTIVITIES SECTION VARIABLES
+const activityButtonLeft = document.getElementById("activity-button-left")
+const activityButtonRight = document.getElementById("activity-button-right")
+const firstActivityRowImages = document.getElementsByClassName("first-activity-row-images")
+const secondActivityRowImages = document.getElementsByClassName("second-activity-row-images")
+const thirdActivityRowImages = document.getElementsByClassName("third-activity-row-images")
+
+const activityRowArray = [firstActivityRowImages, secondActivityRowImages, thirdActivityRowImages]
+
 // BOTTOM SCROLL TO TOP BUTTON
 const backToTopButton = document.getElementById("back-to-top-button")
 
@@ -12,53 +21,15 @@ const backToTopButton = document.getElementById("back-to-top-button")
 EVENT LISTENERS
 ================================================================================ */
 
+//Event Listeners for activities left and right buttons
+activityButtonLeft.addEventListener("click", leftButton)
+activityButtonRight.addEventListener("click", rightButton)
+
 // Event listener for scroll to top button
 backToTopButton.addEventListener("click", scrollTop)
-
 /* =============================================================================
 FUNCTIONS
 ================================================================================ */
-
-const activityButtonLeft = document.getElementById("activity-button-left")
-const activityButtonRight = document.getElementById("activity-button-right")
-
-const firstActivityRowImages = document.getElementsByClassName("first-activity-row-images")
-
-activityButtonLeft.addEventListener("click", fadeOutImages)
-activityButtonRight.addEventListener("click", fadeOutImages)
-
-
-
-
-function fadeOutImages() {
-  for(let i=0; i<firstActivityRowImages.length; i++) {
-    firstActivityRowImages[i].classList.add("fade-out")
-    setTimeout(function(){
-    firstActivityRowImages[i].classList.add("hide")
-  }, 400)
-  }
-}
-
-function fadeInImages() {
-    for(let i=0; i<firstActivityRowImages.length; i++) {
-      firstActivityRowImages[i].classList.remove("fade-out")
-      firstActivityRowImages[i].classList.add("fade-in")
-      setTimeout(function(){
-        firstActivityRowImages[i].classList.remove("hide")
-      }, 300)
-    }
-}
-
-
-
-
-
-
-
-// Function to hide all images
-
-// when right click button is pressed if firstactivityrowimages.classList.includes("active") then run the hide all images function and load secondRowImages
-// if second rowImages.classList.includes("active") run hide all images and then run load thirdRowimages etc
 
 
 // NAVIGATION FUNCTIONS
@@ -76,6 +47,69 @@ function removeActiveNavClass() {
   let current = document.getElementsByClassName("active");
   if (current.length > 0) {
     current[0].className = current[0].className.replace("active", "");
+  }
+}
+
+// ACTIVITIES SECTION FUNCTIONS
+
+//Function for right activities button
+function rightButton() {
+  if (!firstActivityRowImages[0].classList.contains("hide")) {
+    fadeOutImages()
+    fadeInImages(secondActivityRowImages)
+    return
+    }
+  if (!secondActivityRowImages[0].classList.contains("hide")) {
+    fadeOutImages()
+    fadeInImages(thirdActivityRowImages)
+    return
+    }
+  if (!thirdActivityRowImages[0].classList.contains("hide")) {
+    fadeOutImages()
+    fadeInImages(firstActivityRowImages)
+    return
+    }
+  }
+
+//Function for left activities button
+function leftButton() {
+  if (!firstActivityRowImages[0].classList.contains("hide")) {
+    fadeOutImages()
+    fadeInImages(thirdActivityRowImages)
+    return
+    }
+  if (!secondActivityRowImages[0].classList.contains("hide")) {
+    fadeOutImages()
+    fadeInImages(firstActivityRowImages)
+    return
+    }
+  if (!thirdActivityRowImages[0].classList.contains("hide")) {
+    fadeOutImages()
+    fadeInImages(secondActivityRowImages)
+    return
+    }
+  }
+
+//Function to fade out images
+function fadeOutImages() {
+  for(let i = 0; i < activityRowArray.length; i++) {
+    for (let j = 0; j < activityRowArray[i].length; j++) {
+      activityRowArray[i][j].classList.add("fade-out")
+      setTimeout(function(){
+        activityRowArray[i][j].classList.add("hide")
+      }, 400)
+    }
+  }
+}
+
+//Function to fade in images
+function fadeInImages(rowName) {
+  for (let i = 0; i < rowName.length; i++) {
+  rowName[i].classList.remove("fade-out")
+  rowName[i].classList.add("fade-in")
+  setTimeout(function(){
+    rowName[i].classList.remove("hide")
+    }, 400)
   }
 }
 
