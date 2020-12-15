cc5df383a89839c33de50207f57f431b382745e3
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2020-12-15 13:26:26 +0000

    Fade in and fade out functions and variables for activities photo area
    - Added fade in function
    - Added fade out function
    - Added variables for images in first row of services photos

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const btnContainer = document.getElementById("navbarNav")
 const btns = btnContainer.getElementsByClassName("nav-link")
 
 // BOTTOM SCROLL TO TOP BUTTON
-const backToTopButton = document.getElementById("back-to-top-button");
+const backToTopButton = document.getElementById("back-to-top-button")
 
 /* =============================================================================
 EVENT LISTENERS
@@ -18,6 +18,48 @@ backToTopButton.addEventListener("click", scrollTop)
 /* =============================================================================
 FUNCTIONS
 ================================================================================ */
+
+const activityButtonLeft = document.getElementById("activity-button-left")
+const activityButtonRight = document.getElementById("activity-button-right")
+
+const firstActivityRowImages = document.getElementsByClassName("first-activity-row-images")
+
+activityButtonLeft.addEventListener("click", fadeOutImages)
+activityButtonRight.addEventListener("click", fadeOutImages)
+
+
+
+
+function fadeOutImages() {
+  for(let i=0; i<firstActivityRowImages.length; i++) {
+    firstActivityRowImages[i].classList.add("fade-out")
+    setTimeout(function(){
+    firstActivityRowImages[i].classList.add("hide")
+  }, 400)
+  }
+}
+
+function fadeInImages() {
+    for(let i=0; i<firstActivityRowImages.length; i++) {
+      firstActivityRowImages[i].classList.remove("fade-out")
+      firstActivityRowImages[i].classList.add("fade-in")
+      setTimeout(function(){
+        firstActivityRowImages[i].classList.remove("hide")
+      }, 300)
+    }
+}
+
+
+
+
+
+
+
+// Function to hide all images
+
+// when right click button is pressed if firstactivityrowimages.classList.includes("active") then run the hide all images function and load secondRowImages
+// if second rowImages.classList.includes("active") run hide all images and then run load thirdRowimages etc
+
 
 // NAVIGATION FUNCTIONS
 
